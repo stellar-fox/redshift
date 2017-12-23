506c8a7d144206336ace98b5ed10a360f14716d2
syntaxval <syntaxval@users.noreply.github.com>
2017-12-23 23:26:14 +0100

    Add stellar-base and sjcl libs

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Stellar Fox</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          More to come very very soon.
         </p>
       </div>
     );

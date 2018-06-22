9135351f60ddb5ed9d856aaccac4898911b6c74b
syntaxval <syntaxval@users.noreply.github.com>
2018-06-22 10:18:43 +0200

    Expose libraries in dev env

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import Content from './frontend/content/Content'
 import Footer from './frontend/footer/Footer'
 import Navbar from './frontend/navbar/Navbar'
 
+import { isObject } from "@xcmats/js-toolbox"
+import { devEnv, dynamicImportLibs } from "./lib/utils"
+
+
+// expose 'redshift' dev. namespace only in dev. environment
+if (devEnv() && isObject(window)) {
+  (async () => {
+  window.rs = {
+    ...await dynamicImportLibs(),
+  }
+  })()
+}
+
 class App extends Component {
   render() {
     return (

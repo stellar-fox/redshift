77d143a43476ef2db64054595cbb04956a2a6c29
drmats <drmats@users.noreply.github.com>
2018-07-12 19:00:44 +0200

    <Input>: relint.

---
+++ src/frontend/Input/index.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+import "./index.css"
+
+
+
+
+// <Input> component
+export default (props) =>
+    <div className="lcars-input">
+        <ul>
+            <li>
+                <label htmlFor="name">{props.label}</label>
+                <input
+                    value={props.value}
+                    type={props.inputType}
+                    maxLength={props.maxLength}
+                    autoComplete={props.autoComplete}
+                    onKeyPress={props.keyPress}
+                    onChange={props.handleChange}
+                />
+                <span>{props.subLabel}</span>
+            </li>
+        </ul>
+    </div>

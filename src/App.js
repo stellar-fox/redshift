426124593da178e42dc3b739be27dedc67c0a1f6
drmats <drmats@users.noreply.github.com>
2018-07-12 14:57:54 +0200

    <App>: linted.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
-import './App.css';
-import Content from './frontend/content/Content'
-import Footer from './frontend/footer/Footer'
-import Navbar from './frontend/navbar/Navbar'
-
+import React, { Fragment } from "react"
+import { BrowserRouter as Router } from "react-router-dom"
 import { isObject } from "@xcmats/js-toolbox"
 import { devEnv, dynamicImportLibs } from "./lib/utils"
 
+import Content from "./frontend/content/Content"
+import Footer from "./frontend/footer/Footer"
+import Navbar from "./frontend/navbar/Navbar"
+
+import "./App.css"
+
+
+
 
 // expose 'redshift' dev. namespace only in dev. environment
-if (devEnv() && isObject(window)) {
-  (async () => {
-  window.rs = {
-    ...await dynamicImportLibs(),
-  }
-  })()
+if (devEnv()  &&  isObject(window)) {
+    (async () => {
+        window.rs = {
+            ...(await dynamicImportLibs()),
+        }
+    })()
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <div>
+
+
+
+// <App> component
+export default () =>
+    <Router>
+        <Fragment>
             <Navbar />
             <Content />
             <Footer />
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}
-
-export default App;
+        </Fragment>
+    </Router>

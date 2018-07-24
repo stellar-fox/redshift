168b708421323dd6fbf4c422d1aabd1b9ac4c36a
drmats <drmats@users.noreply.github.com>
2018-07-24 15:00:40 +0200

    Config: changed file structure for npm publishing.

--- src/frontend/RedShiftApp/index.jsx
+++ src/frontend/RedShiftApp/index.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import { isObject } from "@xcmats/js-toolbox"
-import { devEnv, dynamicImportLibs } from "../../lib/utils"
+import { devEnv, dynamicImportLibs } from "../../utils"
 
 import Content from "../Content"
 import Footer from "../Footer"

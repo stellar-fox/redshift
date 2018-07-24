68226dfb7eeb3e90a9ba8f229813dfb2faea0dc1
drmats <drmats@users.noreply.github.com>
2018-07-24 15:20:33 +0200

    lib: added typescript declaration file.

---
+++ src/lib/typings/redshift.d.ts
@@ -0,0 +1,70 @@
+/**
+ * Redshift.
+ * Stellar HD Address Generator.
+ *
+ * @module @stellar-fox/redshift
+ * @license Apache-2.0
+ */
+
+
+
+
+declare module "@stellar-fox/redshift" {
+
+
+
+
+    /**
+     * Entropy preset.
+     */
+    export const ENTROPY: object;
+
+
+
+
+    /**
+     * Languages.
+     */
+    export const LANGUAGE: object;
+
+
+
+
+    /**
+     * Generate mnemonic.
+     */
+    export function genMnemonic (entropy: string, language: string): string;
+
+
+
+
+    /**
+     * Generate hex seed from a given mnemonic.
+     */
+    export function hexSeed (mnemonic: string, passphrase: string): string;
+
+
+
+
+    /**
+     * Generate stellar keypair object from a given seed and pathIndex.
+     */
+    export function keypair (seed: string, pathIndex: number): object;
+
+
+
+
+    /**
+     * Randomly generate object with mnemonic, seed,
+     * keypair, publicKey and secret.
+     */
+    export function random (
+        language: string,
+        passphrase: string,
+        pathIndex: number
+    ): object;
+
+
+
+
+}

62b81ed35c9b9a8fb509de8eb4741ab9b732b32a
drmats <drmats@users.noreply.github.com>
2018-10-01 16:31:49 +0200

    Config: '.babelrc' replaced with '.babelrc.js'.

---
+++ src/lib/.babelrc.js
@@ -0,0 +1,70 @@
+module.exports = function (api) {
+    api.cache.using(() => process.env.BABEL_ENV);
+    console.log("Compiling for", "'" + api.env() + "'", "...");
+
+    return {
+        "env": {
+            "commonjs": {
+                "comments": false,
+                "plugins": [
+                    "@babel/plugin-proposal-object-rest-spread",
+                    "@babel/plugin-transform-arrow-functions",
+                    "@babel/plugin-transform-block-scoped-functions",
+                    "@babel/plugin-transform-block-scoping",
+                    "@babel/plugin-transform-computed-properties",
+                    "@babel/plugin-transform-destructuring",
+                    "@babel/plugin-transform-exponentiation-operator",
+                    "@babel/plugin-transform-literals",
+                    "@babel/plugin-transform-modules-commonjs",
+                    "@babel/plugin-transform-parameters",
+                    "@babel/plugin-transform-runtime",
+                    "@babel/plugin-transform-shorthand-properties",
+                    "@babel/plugin-transform-spread",
+                    "@babel/plugin-transform-template-literals"
+                ],
+                "presets": [
+                    [
+                        "@babel/preset-env",
+                        {
+                            "modules": "commonjs",
+                            "shippedProposals": true,
+                            "targets": {
+                                "node": "6.0.0"
+                            }
+                        }
+                    ]
+                ]
+            },
+            "es": {
+                "comments": false,
+                "plugins": [
+                    "@babel/plugin-proposal-object-rest-spread",
+                    "@babel/plugin-transform-arrow-functions",
+                    "@babel/plugin-transform-block-scoped-functions",
+                    "@babel/plugin-transform-block-scoping",
+                    "@babel/plugin-transform-computed-properties",
+                    "@babel/plugin-transform-destructuring",
+                    "@babel/plugin-transform-exponentiation-operator",
+                    "@babel/plugin-transform-literals",
+                    "@babel/plugin-transform-parameters",
+                    "@babel/plugin-transform-runtime",
+                    "@babel/plugin-transform-shorthand-properties",
+                    "@babel/plugin-transform-spread",
+                    "@babel/plugin-transform-template-literals"
+                ],
+                "presets": [
+                    [
+                        "@babel/preset-env",
+                        {
+                            "modules": false,
+                            "shippedProposals": true,
+                            "targets": {
+                                "esmodules": true
+                            }
+                        }
+                    ]
+                ]
+            }
+        }
+    };
+};

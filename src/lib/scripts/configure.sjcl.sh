d33b037a0c193fcaee0724d69054f4849750b2e1
drmats <drmats@users.noreply.github.com>
2018-10-25 22:14:36 +0200

    sjcl config script: brake long line.

--- src/lib/scripts/configure.sjcl.sh
+++ src/lib/scripts/configure.sjcl.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd ./node_modules/sjcl/
-./configure --without-all --with-bitArray --with-codecString --with-codecHex --with-sha512 --with-hmac --compress=none
+./configure \
+    --without-all \
+    --with-bitArray \
+    --with-codecString \
+    --with-codecHex \
+    --with-sha512 \
+    --with-hmac \
+    --compress=none
 make

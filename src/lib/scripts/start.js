bc5bad7a867b2931a4d4b2b930f67096a2e4ec68
drmats <drmats@users.noreply.github.com>
2018-10-25 21:10:32 +0200

    lib: config - scripts, build: lib -> dist.

--- src/lib/scripts/start.js
+++ src/lib/scripts/start.js
@@ -6,6 +6,6 @@
 // inject compiled module to repl context
 Object.assign(
     require("repl").start({}).context, {
-        redshift: require("../lib/index.js"),
+        redshift: require("../dist/index.js"),
     }
 )

af822440bece7def2bee2ab536a28b14fa56dd0c
drmats <drmats@users.noreply.github.com>
2018-10-26 23:10:35 +0200

    Frontend: <Footer> - update.

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -10,8 +10,8 @@ export default () =>
     <div className="footer">
         <footer>
             <div>
-                © &nbsp;&nbsp;<span><b>Stellar Fox</b></span>&nbsp;&nbsp; 🦊 &nbsp;&nbsp;2017-2018.
+                &nbsp;© &nbsp;&nbsp;<span class="footersfox"><b><a href="https://stellarfox.net">Stellar Fox</a></b></span>&nbsp;&nbsp; <span class="footermoji" aria-label="fox" role="img">🦊</span> &nbsp;&nbsp;2017-2018.
             </div>
-            <div className="right">Made with &nbsp;♥&nbsp; in Berlin | Bangkok</div>
+            <div className="right">Made with &nbsp;<span class="footerheart" aria-label="love" role="img">❤</span>&nbsp; in Wrocław | Berlin | Bangkok &nbsp;&nbsp; &nbsp;<span class="footermoji" aria-label="rocket" role="img">🚀</span>&nbsp;</div>
         </footer>
     </div>

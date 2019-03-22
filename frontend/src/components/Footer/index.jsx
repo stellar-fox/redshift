94b1b89dcab3fd0d74341dd6d6603d8b0eea628d
syntaxval <syntaxval@users.noreply.github.com>
2019-03-22 10:19:51 +0100

    Update dependencies. Update footer and logo.

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -11,7 +11,7 @@ export default () =>
     <div className="footer">
         <footer>
             <div>
-                &nbsp;© &nbsp;&nbsp;<span className="footersfox"><b><a href="https://stellarfox.net">Stellar Fox</a></b></span>&nbsp;&nbsp; <span className="footermoji" aria-label="fox" role="img">🦊</span> &nbsp;&nbsp;2017-2018.
+                &nbsp;© &nbsp;<span className="footersfox"><b><a href="https://stellarfox.net">Stellar Fox</a></b></span>&nbsp;&nbsp;2017-2019.
             </div>
             <div className="right">
                 Made with &nbsp;<span className="footerheart" aria-label="love" role="img">❤</span>&nbsp; in { array.draw(["Wrocław", "Berlin", "Bangkok"]) }. ver. <b>{ version }</b> &nbsp;&nbsp; &nbsp;<span className="footermoji" aria-label="rocket" role="img">🚀</span>&nbsp;

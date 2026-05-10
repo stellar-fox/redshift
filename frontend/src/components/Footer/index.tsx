b303c55fc30a1089a2d5070570a481af738c2223
syntaxval <29269124+syntaxval@users.noreply.github.com>
2026-05-10 09:21:31 +0200

    fix: manifest path for github pages and dynamic footer year

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -12,7 +12,7 @@ export default () =>
     <div className="footer">
         <footer>
             <div>
-                &nbsp;© &nbsp;<span className="footersfox"><b><a href="https://stellarfox.net">Stellar Fox</a></b></span>&nbsp;&nbsp;2017-2019.
+                &nbsp;© &nbsp;<span className="footersfox"><b><a href="https://stellarfox.net">Stellar Fox</a></b></span>&nbsp;&nbsp;2017-{new Date().getFullYear()}.
             </div>
             <div className="right">
                 Made with &nbsp;<span className="footerheart" aria-label="love" role="img">❤</span>&nbsp; in { array.draw(["Wrocław", "Berlin", "Bangkok"]) }. ver. <b>{ version }</b> &nbsp;&nbsp; &nbsp;<span className="footermoji" aria-label="rocket" role="img">🚀</span>&nbsp;
